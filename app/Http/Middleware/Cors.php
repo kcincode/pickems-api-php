d953d43b3bbd8e49f41dd2fc799945f2f8f3dd4a
Nick Felicelli <nick.felicelli@gmail.com>
2016-10-21 14:44:13 -0400

    Fixing cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,14 +9,15 @@ class Cors
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:4200');
+        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:4200, https://pickems.surge.sh');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
