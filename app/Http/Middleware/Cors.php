2d46a2535502b26b06f3bfb7756393f10536f22f
Nick Felicelli <nick.felicelli@gmail.com>
2016-10-21 14:49:09 -0400

    Fixing cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,9 +17,20 @@ class Cors
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:4200, https://pickems.surge.sh');
+
+        $response->headers->set('Access-Control-Allow-Origin', $this->getFrontendUrl());
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         return $response;
+    }
+
+    private function getFrontendUrl()
+    {
+        switch (App::environment()) {
+        case 'prod':
+          return 'https://pickems.surge.sh';
+        default:
+          return 'http://localhost:4200';
+      }
     }
 }
